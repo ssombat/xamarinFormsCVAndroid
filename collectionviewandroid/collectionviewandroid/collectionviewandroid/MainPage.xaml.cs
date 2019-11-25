9a9d6055f8cbdf2d21493834baf9070e5fb10b06
ssombat <sombatsteven@gmail.com>
2019-11-25 14:37:59 -0500

    Collection View Repo
    Repo

---
+++ collectionviewandroid/collectionviewandroid/collectionviewandroid/MainPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace collectionviewandroid
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        List<object> someList = new List<object>();
+        public MainPage()
+        {
+            BindingContext = this;
+            for (int i = 0; i < 30; i++)
+            {
+                someList.Add(new object());
+            }
+            InitializeComponent();
+            myCollectionView.ItemsSource = someList;
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            myframe.IsVisible = !myframe.IsVisible;
+        }
+
+    }
+}
